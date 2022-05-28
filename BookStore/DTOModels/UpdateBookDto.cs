28cc9b3b56df8aedea34e746c754bbb9b8e74eb2
PawelKeskaHTD <pawel.keska@htdevelopers.com>
2022-05-28 06:07:37 +0200

    Add updatebookdto.

---
+++ BookStore/DTOModels/UpdateBookDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.DTOModels;
+
+public class UpdateBookDto
+{
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Author { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public string Publisher { get; set; }
+    [Required]
+    public int Year { get; set; }
+    public int Pages { get; set; }
+    public string ImageUrl { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+}
