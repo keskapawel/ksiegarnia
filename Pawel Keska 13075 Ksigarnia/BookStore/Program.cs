9f01a59534d76599493b11f24ffead8093884794
PawelKeskaHTD <pawel.keska@htdevelopers.com>
2022-06-10 19:34:00 +0200

    Add authorization.

--- Pawel Keska 13075 Ksigarnia/BookStore/Program.cs
+++ Pawel Keska 13075 Ksigarnia/BookStore/Program.cs
@@ -1,12 +1,44 @@
 using System.Reflection;
+using System.Text;
+using BookStore;
 using BookStore.DataBase;
+using BookStore.DataBaseEntities;
+using BookStore.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+var authorizationSettings = new AuthorizationSettings();
+builder.Configuration.GetSection("Authentication").Bind(authorizationSettings);
+builder.Services.AddSingleton(authorizationSettings);
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authorizationSettings.JwtIssuer,
+        ValidAudience = authorizationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authorizationSettings.JwtKey)),
+    };
+});
+
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserContextService, UserContextService>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,9 +62,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
