c243cdf6c690735faf969b1386ddf7eb0806b61b
PawelKeskaHTD <pawel.keska@htdevelopers.com>
2022-06-10 19:36:16 +0200

    Restore file.

---
+++ Pawel Keska 13075 Ksigarnia/BookStore/Services/BookService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BookStore.DataBase;
+using BookStore.DTOModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookStore.Services;
+
+public class BookService
+{
+    private readonly BookStoreDbContext _context;
+    private readonly IMapper _mapper;
+
+    public BookService(BookStoreDbContext context,IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
