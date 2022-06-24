0ca5580c2199cb97bb508ddf6404a1e4af062332
PawelKeskaHTD <pawel.keska@htdevelopers.com>
2022-06-24 20:16:45 +0200

    Add AccountController.

---
+++ Pawel Keska 13075 Ksigarnia/BookStore/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.DTOModels;
+using BookStore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers;
+
+[ApiController]
+[Route("api/account")]
+public class AccountController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+
+    public AccountController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody]UserRegisterDto dto)
+    {
+        _userService.RegisterUser(dto);
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public ActionResult Login([FromBody]UserLoginDto dto)
+    {
+        string result = _userService.Login(dto);
+        return Ok(result);
+    }
+}
